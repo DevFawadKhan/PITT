2054d965ac72e7afe2cca36ef72ec441bede3e87
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-12-13 19:29:04 +0500

    tailwind error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Home from "./pages/Home.jsx";
-
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import "./App.css";
 export default function App() {
   return (
     <>
---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer bg-gray">
+      <div className="container">
+        <div className="grid grid-rows-4 grid-flow-col gap-4">
+          <div>
+            01 Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+            delectus quis esse culpa in veritatis.
+          </div>
+
+          <div>
+            09 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
+            sed nihil, vel velit et harum.
+          </div>
+          <div>
+            09 Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
+            sed nihil, vel velit et harum.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
