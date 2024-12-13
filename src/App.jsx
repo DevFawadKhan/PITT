28ed27e1112d6e28b7c7cdafec981287b3407ca0
DevFawadKhan <mfawadkhan05@gmail.com>
2024-12-13 06:57:12 -0700

    set main file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Home from "./pages/Home";
+import Home from "./pages/Home.jsx";
 
 export default function App() {
   return (
---
+++ src/components/Exclusive.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Exclusive() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Exclusive
