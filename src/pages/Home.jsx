642ee49435317dfad154f3a875d039ea3c809fe8
Huzaifa Afraz <huzaifaafraz90@gmail.com>
2024-12-16 19:30:39 +0500

    footer complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,5 @@ import React from 'react'
 import Footer from "../components/Footer";
 
 export default function Home() {
-  return (
-    <div className="bg-blue-500 text-white p-4">
-      <Footer />
-    </div>
-  );
+  return <>{/* <Footer /> */}</>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {
       container: {
-        center: true, // Center the container
-        padding: '2rem', // Default padding for the container
+        center: true,
       },
     },
   },
   plugins: [],
-}
+};
