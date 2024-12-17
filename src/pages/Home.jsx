b4621da002098cf15652c011c06175b74cb089bb
DevFawadKhan <mfawadkhan05@gmail.com>
2024-12-17 06:52:14 -0700

    Done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 // import Exclusive from '../components/Exclusive';
-import Whychoose from "../components/Whychoose";
+// import Whychoose from "../components/Whychoose";
+import Worldclassic from "../components/Worldclassic";
 export default function Home() {
   return (
 <>
 {/* <Exclusive></Exclusive> */}
-<Whychoose></Whychoose>
+{/* <Whychoose></Whychoose> */}
+<Worldclassic></Worldclassic>
 </>
   );
 }
